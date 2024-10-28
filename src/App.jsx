efa4084f90d6d6a6b3d25025399fc444b7b8f180
Rafayet Hossen <hossenmdrafayet@gmail.com>
2024-10-28 21:34:08 +0600

    Added React Toastify perfectly working and coditional toastify rendering successfully

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,42 @@
-import Header from './Header'
-import Banner from './Banner'
-import { useEffect, useState } from 'react';
-import Players from './Players';
+import Header from "./Header";
+import Banner from "./Banner";
+import { useEffect, useState } from "react";
+import Players from "./Players";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 function App() {
   const [coins, setCoins] = useState(0);
-  const handleAddCoins = () => {
+  const handleAddCoins = (event) => {
+    event.preventDefault(); 
     setCoins(coins + 1000000);
+    notifySuccess();
   };
+
+  const notifySuccess = () => {
+    toast.success("Coin Added successfully!", {
+      position: "top-center",
+      autoClose: 500,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
+
   useEffect(() => {
-    document.title = 'Welcome to Dream11'
-  },[])
+    document.title = "Welcome to Dream11";
+  }, []);
+
   return (
     <>
-      <Header coins={coins}></Header>
-      <Banner handleAddCoins = {handleAddCoins}></Banner>
-      <Players></Players>
+      <Header coins={coins} />
+      <Banner handleAddCoins={handleAddCoins} />
+      <Players coins={coins} setCoins={setCoins} />
+      <ToastContainer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Players.jsx
+++ src/Players.jsx
@@ -1,20 +1,36 @@
 import { useEffect, useState } from "react";
 import Player from "./Player";
+import PropTypes from "prop-types";
 
-const Players = () => {
-    const [players,setPlayers] = useState([])
-    useEffect(() => {
-        fetch('../public/player.json')
-        .then(res => res.json())
-        .then(data => setPlayers(data))
-    },[])
-    return (
-      <div className="grid grid-cols-1 lg:grid-cols-3 w-11/12 mx-auto gap-10 mt-10 justify-center">
-        {players.map((player) => (
-          <Player key={player.id} player={player} />
-        ))}
-      </div>
-    );
+const Players = ({ coins, setCoins }) => {
+  const [players, setPlayers] = useState([]);
+
+  useEffect(() => {
+    fetch("../public/player.json")
+      .then((res) => res.json())
+      .then((data) => setPlayers(data));
+  }, []);
+  const onSelectPlayer = (price) => {
+    setCoins((prevCoins) => prevCoins - price);
+  };
+
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-3 w-11/12 mx-auto gap-10 mt-10 justify-center">
+      {players.map((player) => (
+        <Player
+          key={player.id}
+          player={player}
+          coins={coins}
+          onSelectPlayer={onSelectPlayer}
+        />
+      ))}
+    </div>
+  );
+};
+
+Players.propTypes = {
+  coins: PropTypes.number.isRequired,
+  setCoins: PropTypes.func.isRequired, 
 };
 
 export default Players;
